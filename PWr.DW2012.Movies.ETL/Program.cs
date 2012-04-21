48a769759cbccb6a110f5bcb7ed51b874fa5a274
Marcin Wisnicki <mwisnicki@gmail.com>
2012-04-21 20:31:31 +0200

    Initial import.

---
+++ PWr.DW2012.Movies.ETL/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PWr.DW2012.Movies {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
