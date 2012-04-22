c1e3ad024f60306760c81bb5afdfb8e72e33b09d
Marcin Wisnicki <mwisnicki@gmail.com>
2012-04-22 23:16:22 +0200

    Fix cleaner script.

--- htmlcleaner.cmd
+++ htmlcleaner.cmd
@@ -2,4 +2,5 @@ set HtmlCleanerCmd=java -Dfile.encoding=utf-8 -jar ../htmlcleaner-2.2.jar
 set DestDir=Clean
 
 cd Data
+mkdir %DestDir%
 for /f %%f in ('dir /b *.htm') do %HtmlCleanerCmd% src=%%f dest=%DestDir%\%%f
